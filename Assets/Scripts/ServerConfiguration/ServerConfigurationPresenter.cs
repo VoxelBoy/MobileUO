aa73de7e6bcf3fd2b43ee9310f7120e41bb3a374
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2020-05-27 00:25:02 +0200

    Don't show the Patreon button on iOS as Apple doesn't like it.

--- Assets/Scripts/ServerConfiguration/ServerConfigurationPresenter.cs
+++ Assets/Scripts/ServerConfiguration/ServerConfigurationPresenter.cs
@@ -4,10 +4,13 @@ public class ServerConfigurationPresenter : MonoBehaviour
 {
     [SerializeField] private ServerConfigurationListPresenter serverConfigurationListPresenter;
     [SerializeField] private ServerConfigurationEditPresenter serverConfigurationEditPresenter;
+    [SerializeField] private GameObject patreonParent;
 
     private void Start()
     {
         SwitchToList();
+        
+        patreonParent.SetActive(Application.platform != RuntimePlatform.IPhonePlayer);
 
         serverConfigurationListPresenter.AddNewConfigurationButtonClicked += OnAddNewConfigurationButtonClicked;
         serverConfigurationListPresenter.EditButtonClicked += OnEditButtonClicked;
