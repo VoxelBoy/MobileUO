17f4b2c55d5cf7b08522f87ee18779aa8d654d43
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2020-07-12 14:16:54 +0200

    Added commented out SD card related code that I will use later

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -38,10 +38,53 @@ public class Init : MonoBehaviour
         ServerConfigurationModel.Initialize(supportedServerConfigurations);
         
         StateManager.GoToState<BootState>();
+
+        //Commented out for now, will be used later once I have an SD card to test with
+        //GetAndroidExternalFilesDir();
     }
 
     private void OnShowDebugConsoleChanged(int currentValue)
     {
         inGameDebugConsoleCanvas.enabled = currentValue == (int) PreferenceEnums.ShowDebugConsole.On;
+    }
+    
+    private static string GetAndroidExternalFilesDir()
+    {
+        #if UNITY_ANDROID && !UNITY_EDITOR
+        using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject context = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                // Get all available external file directories (emulated and sdCards)
+                AndroidJavaObject[] externalFilesDirectories = context.Call<AndroidJavaObject[], string>("getExternalFilesDirs", null);
+                AndroidJavaObject emulated = null;
+                AndroidJavaObject sdCard = null;
+
+                for (int i = 0; i < externalFilesDirectories.Length; i++)
+                {
+                    AndroidJavaObject directory = externalFilesDirectories[i];
+                    using (AndroidJavaClass environment = new AndroidJavaClass("android.os.Environment"))
+                    {
+                        // Check which one is the emulated and which the sdCard.
+                        bool isRemovable = environment.CallStatic<bool> ("isExternalStorageRemovable", directory);
+                        bool isEmulated = environment.CallStatic<bool> ("isExternalStorageEmulated", directory);
+                        if (isEmulated)
+                            emulated = directory;
+                        else if (isRemovable && isEmulated == false)
+                            sdCard = directory;
+                    }
+                }
+                
+                // Return the sdCard if available
+                if (sdCard != null)
+                {
+                    return sdCard.Call<string>("getAbsolutePath");
+                }
+
+                return emulated.Call<string>("getAbsolutePath");
+            }
+        }
+        #endif
+        return string.Empty;
     }
 }
